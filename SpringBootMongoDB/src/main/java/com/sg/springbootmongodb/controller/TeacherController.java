8c83cc6268e37a220fc76df9acbb17642d617aae
hxinqc <117105117+hxinqc@users.noreply.github.com>
2023-01-11 14:32:47 -0500

    Add crud controller

--- SpringBootMongoDB/src/main/java/com/sg/springbootmongodb/controller/TeacherController.java
+++ SpringBootMongoDB/src/main/java/com/sg/springbootmongodb/controller/TeacherController.java
@@ -1,14 +1,15 @@
 package com.sg.springbootmongodb.controller;
 
+import com.sg.springbootmongodb.exception.RecordNotFoundException;
 import com.sg.springbootmongodb.model.Teacher;
 import com.sg.springbootmongodb.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:5555")
 @RestController
@@ -21,5 +22,38 @@ public class TeacherController {
     @GetMapping("/teacher")
     public List<Teacher> getAllEmployees() {
         return service.findAll();
+    }
+
+    @PostMapping("/teacher")
+    public Teacher createTeacher(@RequestBody Teacher teacher) {
+        return service.save(teacher);
+    }
+
+    @GetMapping("/teacher/{id}")
+    public ResponseEntity<Teacher> getTeacherById(@PathVariable(value = "id") String id)
+            throws RecordNotFoundException {
+        return ResponseEntity.ok().body(service.getTeacherById(id));
+    }
+
+    @PutMapping("/teacher/{id}")
+    public ResponseEntity<Teacher> updateTeacherById(@PathVariable(value = "id") String id,
+             @RequestBody Teacher teacherU) throws RecordNotFoundException {
+        Teacher teacher = service.getTeacherById(id);
+        teacher.setFirstName(teacherU.getFirstName());
+        teacher.setLastName(teacherU.getLastName());
+        teacher.setIsActive(teacherU.getIsActive());
+        service.save(teacher);
+        return ResponseEntity.ok().body(teacher);
+    }
+
+    @DeleteMapping("/teacher/{id}")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") String id)
+            throws RecordNotFoundException {
+        Teacher teacher = service.getTeacherById(id);
+
+        service.deleteTeacher(teacher);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
--- SpringBootMongoDB/src/main/java/com/sg/springbootmongodb/service/TeacherService.java
+++ SpringBootMongoDB/src/main/java/com/sg/springbootmongodb/service/TeacherService.java
@@ -1,5 +1,6 @@
 package com.sg.springbootmongodb.service;
 
+import com.sg.springbootmongodb.exception.RecordNotFoundException;
 import com.sg.springbootmongodb.model.Teacher;
 import com.sg.springbootmongodb.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,19 @@ public class TeacherService {
 
     public List<Teacher> findAll() {
         return teacherRepository.findAll();
+    }
+
+    public Teacher save(Teacher teacher) {
+        return teacherRepository.save(teacher);
+    }
+
+    public Teacher getTeacherById(String id) throws RecordNotFoundException{
+        Teacher teacher = teacherRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("Teacher not found with id: " + id));
+        return teacher;
+    }
+
+    public void deleteTeacher(Teacher teacher) {
+        teacherRepository.delete(teacher);
     }
 }
